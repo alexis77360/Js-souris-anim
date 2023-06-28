1d903f7edf39cf4ce430a687b933b968e41b5ac6
alexis77360 <84974466+alexis77360@users.noreply.github.com>
2023-06-28 16:34:41 +0200

    optimisation

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
-// Créer 3 ronds de tailles différentes (dont un qui remplacera la souris)
+const mouses = document.querySelectorAll('.mouse');
 
 
 
-// Ajouter un événement sur la fenetre (window) puis animer la position de ces ronds (top, left injecter "e")
+window.addEventListener('mousemove', (e) => {
 
-// S'assurer que les liens sont clickables
-
-// Donner un style de transparence aux 2 plus gros ronds (mix-blend-mode)
+    //?Pour chaque souris, on va changer la position en fonction de la position de la souris
+    mouses.forEach((mouse) => {
+        mouse.style.left = `${e.pageX}px`;
+        mouse.style.top = `${e.pageY}px`;
+    });
+    }
+);
